a7651cc6fd79108bd8146e6281be8923110a16f7
nicholas-camp <cmpnik@gmail.com>
2025-04-26 15:31:43 +1200

    Configure project for Github Pages deployment

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,7 +3,7 @@ const projects = [
         name: "Portfolio Site",
         tags: ["Vite", "React", "Typescript"],
         description: "A fully responsive, modern portfolio built with Vite, React, TypeScript, and styled with Tailwind CSS. It showcases my projects, technical skills, and experience, providing an interactive and visually engaging way to explore my work.",
-        github: "https://github.com/nicholas-camp/my-portfolio",
+        github: "https://github.com/nicholas-camp/nicholas-camp.github.io",
     }
 ];
 
